23854697da8ec8153b50184b3ebecd205342cef2
TonnyMusembi <musembutonnyj@gmail.com>
2023-10-03 11:57:25 +0300

    add

--- api.go
+++ api.go
@@ -15,6 +15,11 @@ func main() {
 			"message": "Hello, test api!",
 		})
 	})
+	r.POST("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "test", 
+		})
+	})
 
 	// Start the server on port 8080
 	r.Run(":8089")
